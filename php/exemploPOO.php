631c5f2c09e9eb7dd2ced38aed020a36c62d9eba
Lucas Bertuol <151688160+bertua@users.noreply.github.com>
2024-03-28 00:04:41 +0000

    a

--- php/exemploPOO.php
+++ php/exemploPOO.php
@@ -8,6 +8,19 @@ $p->setidade($idade);
 echo "Pessoa: " . $p->getNome() . "<br> Idade: " . $p->getIdade();
 
 $p->setNome("João");
-echo $p->falar();
+echo "<br>" . $p->falar();
+
+$p->setEmail("qwer@asdf.com");
+$p->setCelular("54 965413215");
+$p->setHora("15:45");
+$p->setSite("asdfgfd.com");
+$p->setSenha("65464565");
+$p->setData("12/05/2000");
+$p->setCor("preto");
+$p->setFilhos("2");
+$p->setPeso(80);
+$p->setSexo("M");
+$p->setEstado("SC");
+$p->setMensagem("asdfgtasdfhsdfgh");
 
 ?>
--- php/pessoa.class.php
+++ php/pessoa.class.php
@@ -2,6 +2,18 @@
 class Pessoa {
     private $nome;
     private $idade;
+    private $email;
+    private $celular;
+    private $hora;
+    private $site;
+    private $senha;
+    private $data;
+    private $cor;
+    private $filhos;
+    private $peso;
+    private $sexo;
+    private $estado;
+    private $mensagem;
 
     function __construct($nome) {
         $this->nome = $nome;
@@ -22,6 +34,116 @@ class Pessoa {
     public function getIdade() {
         return $this->idade;
     }
+
+    public function setEmail($email) {
+        $this->email = $email;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
+
+    public function setCelular($celular) {
+        $this->celular = $celular;
+    }
+
+    public function getCelular() {
+        return $this->celular;
+    }
+
+    public function setHora($hora) {
+        $this->hora = $hora;
+    }
+
+    public function getHora() {
+        return $this->hora;
+    }
+
+    public function setSite($site) {
+        $this->site = $site;
+    }
+
+    public function getSite() {
+        return $this->site;
+    }
+
+    public function setSenha($senha) {
+        $this->senha = $senha;
+    }
+
+    public function getSenha() {
+        return $this->senha;
+    }
+
+    public function setData($data) {
+        $this->data = $data;
+    }
+
+    public function getData() {
+        return $this->data;
+    }
+
+    public function setCor($cor) {
+        $this->cor = $cor;
+    }
+
+    public function getCor() {
+        return $this->cor;
+    }
+
+    public function setFilhos($filhos) {
+        $this->filhos = $filhos;
+    }
+
+    public function getFilhos() {
+        return $this->filhos;
+    }
+
+    public function setPeso($peso) {
+        $this->peso = $peso;
+    }
+
+    public function getPeso() {
+        return $this->peso;
+    }
+
+    public function setSexo($sexo) {
+        $this->sexo = $sexo;
+    }
+
+    public function getSexo() {
+        return $this->sexo;
+    }
+
+    public function setEstado($estado) {
+        $this->estado = $estado;
+    }
+
+    public function getEstado() {
+        return $this->estado;
+    }
+
+    public function setMensagem($mensagem) {
+        $this->mensagem = $mensagem;
+    }
+
+    public function getMensagem() {
+        return $this->mensagem;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     public function falar(){
         $nomedaPessoa = $this->nome;

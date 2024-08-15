8cde2f4ac6cdcf3ac1eeb77250b8572aa05d0c97
Lucas Bertuol <151688160+bertua@users.noreply.github.com>
2024-08-14 21:41:42 -0300

    Add files via upload

--- janelinhas/src/main/java/com/mycompany/janelinhas/Janelinhas.java
+++ janelinhas/src/main/java/com/mycompany/janelinhas/Janelinhas.java
@@ -7,8 +7,10 @@ import javax.swing.*;
 public class Janelinhas {
 
     public static void main(String[] args) {
-        PrimeiraJanela app = new PrimeiraJanela();
+        //PrimeiraJanela app = new PrimeiraJanela();
+        //ExemploLabel app = new ExemploLabel();
+        Atividade app = new Atividade();
         app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-       
+        
     }
 }

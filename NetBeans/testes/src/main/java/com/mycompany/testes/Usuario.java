b86d2ee69653edd5b5cddb6769fc03faf948e3b2
Lucas Bertuol <151688160+bertua@users.noreply.github.com>
2024-10-24 21:17:19 -0300

    Add files via upload

---
+++ NetBeans/testes/src/main/java/com/mycompany/testes/Usuario.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.testes;
+
+/**
+ *
+ * @author 03045812082
+ */
+public class Usuario {
+    
+    private String usuario;
+    private String nome;
+    private String email;
+    private String senha;
+    
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
